634ea5961a26bb608a4b53b3b3b4b594e4cf1d02
Matheus Rodrigues <matheus.g.rodrigues98@gmail.com>
2018-09-29 16:27:21 -0300

    select das ordens adicionados

--- site/app/public/js/app.js
+++ site/app/public/js/app.js
@@ -1,5 +1,5 @@
 function getOrders() {
-    const xhttp = new XMLHttpRequest();
+    var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if(this.status == 200 && this.readyState == 4) {
             mountOrdersTable(JSON.parse(this.response));
@@ -11,14 +11,32 @@ function getOrders() {
 }
 
 function mountOrdersTable(resp) {
-    const table = document.querySelector('#order-table-body');
+    var table = document.querySelector('#order-table-body');
     resp.data.forEach(function(elem) {
-        const row = document.createElement('tr');
+        var row = document.createElement('tr');
         row.innerHTML = '<td>' + elem.client + '</td><td>' + elem.storage + '</td><td>' + elem.product + '</td><td><button>X</button></td>';
         table.appendChild(row);
     });
 }
 
+function toggleModal() {
+    var modal = document.querySelector('.add-order-modal');
+    var bg = document.querySelector('.modal-bg');
+    if(modal.className == 'add-order-modal') {
+        modal.setAttribute('class', 'add-order-modal visible');
+        bg.setAttribute('class', 'modal-bg visible');
+    } else {
+        modal.setAttribute('class', 'add-order-modal');
+        bg.setAttribute('class', 'modal-bg');
+    }
+}
+
 if(window.location.pathname == '/ordem') {
     getOrders();
+}
+
+if(document.querySelector('.btn-open-modal')) {
+    document.querySelector('.btn-open-modal').addEventListener('click', toggleModal);
+    document.querySelector('.btn-close-modal').addEventListener('click', toggleModal);
+    document.querySelector('.modal-bg').addEventListener('click', toggleModal);
 }
