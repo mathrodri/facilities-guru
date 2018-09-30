f6b853e202c79b7adc45cbd066bc9be03755c908
Matheus Rodrigues <matheus.g.rodrigues98@gmail.com>
2018-09-30 03:01:36 -0300

    comentários do client adicionados

--- api/controllers/Client.js
+++ api/controllers/Client.js
@@ -1,26 +1,34 @@
+// exporta os métodos do client
 module.exports = (app) => {
     return {
+        // define a função all
         all: (req, res) => {
+            // requere o model do client
             const client = app.models.ClientModel;
-
+            // procura todos os clients
             client.find((err, data) => {
                 if(err) {
+                    // envia um json de falha
                     res.json({success: false, message: 'Houve algum erro na requisição dos clientes'});
                 } else {
+                    // envia um json de sucesso com os clients
                     res.json({sucess: true, data: data});
                 }
             });
         },
-
+        // define a funcao new
         new: (req, res) => {
+            // instancia um novo client
             const client = new app.models.ClientModel({
                 name: req.body.client
             });
-
+            // salva o novo client
             client.save((err) => {
                 if(err) {
+                    // envia um json de falha
                     res.json({success: false, message: 'Houve algum erro no cadastro do cliente'});
                 } else {
+                    // envia um json de sucesso
                     res.json({sucess: true, message: 'Cliente salvo com sucesso'});
                 }
             });
