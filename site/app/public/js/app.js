5abaa7456467e9364c34b595f4da5e02e2173181
Matheus Rodrigues <matheus.g.rodrigues98@gmail.com>
2018-09-30 23:47:03 -0300

    filtro das ordens feito

--- site/app/public/js/app.js
+++ site/app/public/js/app.js
@@ -10,13 +10,113 @@ function getOrders() {
     xhttp.send();
 }
 
+function getClients() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if(this.status == 200 && this.readyState == 4) {
+            var resp = JSON.parse(this.response);
+            localStorage.setItem('data', JSON.stringify(resp.data));
+            mountOrdersSelect(resp);
+        }
+    };
+    xhttp.open('GET', 'http://localhost:3000/api/client', true);
+    xhttp.setRequestHeader('accept', 'application/json');
+    xhttp.send();
+}
+
 function mountOrdersTable(resp) {
     var table = document.querySelector('#order-table-body');
     resp.data.forEach(function(elem) {
         var row = document.createElement('tr');
-        row.innerHTML = '<td>' + elem.client + '</td><td>' + elem.storage + '</td><td>' + elem.product + '</td><td><button>X</button></td>';
+        row.setAttribute('data-client', elem.client._id);
+        row.setAttribute('data-facility', elem.facility._id);
+        row.innerHTML = '<td>' + elem.client.name + '</td><td>' + (elem.facility.location.initials.toLowerCase() + '-facility-' + elem.facility.number) + '</td><td>' + elem.product + '</td><td><button>X</button></td>';
         table.appendChild(row);
     });
+}
+
+function mountOrdersSelect(resp) {
+    var clientSelect = document.querySelector('.client-select');
+    var facilitySelect = document.querySelector('.facility-select');
+    var clientsRows = document.querySelector('#order-table-body').children;
+    resp.data.forEach(function(elem) {
+        var option = document.createElement('option');
+        option.innerHTML = elem.name;
+        option.setAttribute('value', elem._id);
+        clientSelect.appendChild(option);
+    });
+    clientSelect.addEventListener('change', function() {
+        if(this.value != 'all') {
+            changeClientFilter(this.value, facilitySelect);
+            facilitySelect.removeAttribute('disabled');
+            clientFilter(this.value, clientsRows);
+        } else {
+            changeClientFilter(this.value, facilitySelect);
+            clientFilter(this.value, clientsRows);
+            facilitySelect.setAttribute('disabled', 'disabled');
+        }
+    });
+    facilitySelect.addEventListener('change', function() {
+        changeFacilityFilter(this.value, clientSelect.value, clientsRows);
+    });
+}
+
+function changeClientFilter(id, facilitySelect) {
+    var facilitySelectLength = facilitySelect.children.length;
+    if(id != 'all') {
+        var data = JSON.parse(localStorage.getItem('data'));
+        var client = findClientFacilities(id, data);
+        for(var i = 1; i < facilitySelectLength; i++) {
+            facilitySelect.removeChild(facilitySelect.children[1]);
+        }
+        client.facilities.forEach(function(elem) {
+            var option = document.createElement('option');
+            option.setAttribute('value', elem._id);
+            option.innerHTML = elem.location.initials.toLowerCase() + '-facility-' + elem.number;
+            facilitySelect.appendChild(option);
+        });
+    } else {
+        for(var i = 1; i < facilitySelectLength; i++) {
+            facilitySelect.removeChild(facilitySelect.children[1]);
+        }
+    }
+}
+
+function changeFacilityFilter(facilityId, clientId, clientsRows) {
+    var clientsRowsVisible = Array.prototype.slice.call(clientsRows).filter(function(elem) {
+        return elem.dataset.client == clientId;
+    });
+    clientsRowsVisible.forEach(function(elem) {
+        if(elem.dataset.facility != facilityId) {
+            elem.style.display = 'none';
+        } else {
+            elem.removeAttribute('style');
+        }
+    });
+}
+
+function findClientFacilities(id, data) {
+    for(var i = 0; i < data.length; i++) {
+        if(data[i]._id == id) {
+            return data[i];
+        }
+    }
+}
+
+function clientFilter(clientId, clientsRows) {
+    if(clientId != 'all') {
+        for(var i = 0; i < clientsRows.length; i++) {
+            if(clientsRows[i].dataset.client != clientId) {
+                clientsRows[i].style.display = 'none';
+            } else {
+                clientsRows[i].removeAttribute('style');
+            }
+        }
+    } else {
+        for(var i = 0; i < clientsRows.length; i++) {
+            clientsRows[i].removeAttribute('style');
+        }
+    }
 }
 
 function toggleModal() {
@@ -33,6 +133,7 @@ function toggleModal() {
 
 if(window.location.pathname == '/ordem') {
     getOrders();
+    getClients();
 }
 
 if(document.querySelector('.btn-open-modal')) {
